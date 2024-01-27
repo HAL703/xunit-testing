653a5378d4fbb869757389b60dc47728877aa7da
HAL703 <eonsago703@protonmail.ch>
2024-01-26 19:34:37 -0500

    Modified code for the test, all pass

--- PrimeService.Tests/obj/Debug/net8.0/PrimeService.Tests.AssemblyInfo.cs
+++ PrimeService.Tests/obj/Debug/net8.0/PrimeService.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeService.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3a00f5402e5238016a0a9f8e74b4f40ce8980fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88c446733b1e8c8f4e93d4c935dfcecc217753aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeService.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeService.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
